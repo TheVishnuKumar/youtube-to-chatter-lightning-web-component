a787b120ebed2fd40f3c0a7319f142923f7033ea
Vishnu Kumar <vishnu.kumar@mtxb2b.com>
2019-05-14 19:30:34 +0530

    Code comment

--- force-app/main/default/lwc/youtube_screen/youtube_screen.js
+++ force-app/main/default/lwc/youtube_screen/youtube_screen.js
@@ -1,3 +1,8 @@
+/**
+ * @author Vishnu Kumar
+ * @email vishnukumarramawat@gmail.com
+ * @desc This JS handles the search of youtube videos and provide the functionality to share on chatter.
+*/
 import { LightningElement,track } from 'lwc';
 import searchVideos from '@salesforce/apex/YouTubeController.searchVideos';
 import shareOnChatter from '@salesforce/apex/YouTubeController.shareOnChatter';
@@ -11,10 +16,16 @@ export default class Youtube_screen extends LightningElement {
     @track viewUrl = '';
     @track chatterTxt = '';
 
+    /**
+     * @author Vishnu Kumar
+     * @email vishnukumarramawat@gmail.com
+     * @desc This method search the videos from youtube.
+    */
     doSearch(){
         this.result = [];
         this.viewUrl = '';
 
+        //If search text is not defined then the defaul search will be made using below code.
         if( !this.searchTxt ){
             this.searchTxt = 'Lightning Web Component 0to1code';
         }
@@ -23,6 +34,7 @@ export default class Youtube_screen extends LightningElement {
         searchVideos({searchString:this.searchTxt})
         .then((result) => {
             this.result = result;
+            //Showing first result in the youtube video player.
 
             if( this.result.length > 0 ){
                 this.showVideoInIframe(this.result[0].videoId);
@@ -34,6 +46,11 @@ export default class Youtube_screen extends LightningElement {
         });
     }
 
+    /**
+     * @author Vishnu Kumar
+     * @email vishnukumarramawat@gmail.com
+     * @desc This method share the current video on chatter.
+    */
     shareOnChatter(){
         if(!this.chatterTxt){
             this.showMessage( { message: 'Message is required to post on chatter.', messageType: 'error', mode: 'pester'} );
@@ -52,6 +69,11 @@ export default class Youtube_screen extends LightningElement {
         });
     }
 
+    /**
+     * @author Vishnu Kumar
+     * @email vishnukumarramawat@gmail.com
+     * @desc This generic method handles the text inputs.
+    */
     handleFormInput(event){
         if( event.target.name === 'youtube-search' ){
             this.searchTxt = event.target.value;
@@ -61,18 +83,29 @@ export default class Youtube_screen extends LightningElement {
         }
     }
 
+    /**
+     * @author Vishnu Kumar
+     * @email vishnukumarramawat@gmail.com
+     * @desc This method set the video url in iframe to play the video.
+    */
     showVideoInIframe(videoId){
         this.viewUrl = 'https://www.youtube.com/embed/'+videoId;
     }
 
+    /**
+     * @author Vishnu Kumar
+     * @email vishnukumarramawat@gmail.com
+     * @desc This method handle the event from chhild item component to set the video url in iframe to play the video.
+    */
     handleVideoChange(event){
         this.showVideoInIframe(event.detail.videoId);
     }
 
-    connectedCallback(){
-        this.doSearch();
-    }
-
+    /**
+     * @author Vishnu Kumar
+     * @email vishnukumarramawat@gmail.com
+     * @desc This is generic method to show toast message.
+    */
     showMessage( { title, message, messageType, mode }) {
         this.dispatchEvent(new ShowToastEvent({
             mode,
@@ -80,5 +113,14 @@ export default class Youtube_screen extends LightningElement {
             message,
             variant: messageType,
         }));
+    }
+
+    /**
+     * @author Vishnu Kumar
+     * @email vishnukumarramawat@gmail.com
+     * @desc Calling doSearch method to search on load.
+    */
+    connectedCallback(){
+        this.doSearch();
     }
 }
